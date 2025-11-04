2555b0dbef47449bc7a1f49995f7dd860a4cc871
Himansu121 <himansukumarrana6@gmail.com>
2025-11-05 00:36:39 +0530

    beginning

---
+++ lucky.py
@@ -0,0 +1,100 @@
+# print("hello")
+# a=1
+# while a<=50:
+#     print(a)
+#     a=a+1
+#     if a%2==0:
+#         print("even:",a) #
+#         if a%4==0:
+#             print(a,"num is multiple of 4:")
+#         else:
+#             print("num is not mul.of 4")
+#     else:
+#        print("odd:",a)
+# days=int(input("enter the number of days:"))
+# years=int(days/365)#days//365
+# print(years)
+# remaining_days=days%365
+# print(remaining_days)
+# weeks=remaining_days//7
+# print(weeks)                           
+# user=int(input("enter amount:"))
+# total_500=user//500
+
+# print(total_500)
+# total_200=user%500
+# print(total_200)   
+# total_100= user%200 
+# print(total_100)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+# total_50= user%100
+# print(total_50)  
+# total_20= user%50
+# print(total_20)  
+# total_10= user%20
+# print(total_10)  
+# total_1=user%10 
+# print(total_1)  
+# user = int(input("Enter amount: "))
+
+# total_500 = user // 500
+# rem_500 = user % 500
+
+# total_200 = rem_500 // 200
+# rem_200 = rem_500 % 200
+
+# total_100 = rem_200 // 100
+# rem_100 = rem_200 % 100
+
+# total_50 = rem_100 // 50
+# rem_50 = rem_100 % 50
+
+# total_20 = rem_50 // 20
+# rem_20 = rem_50 % 20
+
+# total_10 = rem_20 // 10
+# rem_10 = rem_20 % 10
+
+# total_1 = rem_10
+
+# print("500:", total_500)
+# print("200:", total_200)
+# print("100:", total_100)
+# print("50:", total_50)
+# print("20:", total_20)
+# print("10:", total_10)
+# print("1:", total_1)
+#string formating
+# name=input("enter the name:")
+# age=input("what's your age:")
+# print(f"my name is{name}$ my age is{age}")
+#  total_use = int(input("Enter the amount of electricity used: "))
+#  if total_use < 0:
+# #     print("No need to pay electricity bill")
+# # elif total_use <= 100:
+# #     print("Electricity usage is within free limit")
+# # elif 100 < total_use < 600:
+# #     print("Your bill is:", 2 + 7)
+# else:
+#     print("Your bill is: 2 rupees")
+# for i in range(1,100):
+#  if i%2==0:
+#      if i>=50:
+#          break
+#      print(i)
+# print("abc"+1)
+# a=["kjhh"]
+# print(bool(a))
+# i=0 
+# while i>0:
+#     print(i)
+#     i+=1
+#     break
+# class dad():
+#     def phone():
+#         print("dad'phone")
+# class son(dad):
+#     def laptop():
+#         print("son'laptop")  
+#         elder_son= son()
+#         elder_son.laptop()      
+#         elder_son.phone()
