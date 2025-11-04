8afb4e5a5364f58004f6530dfefe11e6a4209c3e
Himansu121 <himansukumarrana6@gmail.com>
2025-11-05 00:40:54 +0530

    beginning and knowing function

---
+++ function.py
@@ -0,0 +1,17 @@
+#function
+# def password_checkeer():
+# def hello():
+#     add=4+5
+#     print(add)
+# hello()    
+# def ele(unit):
+#     unit=input()
+#     ele(unit)
+#232424232342
+def student_data(name,roll_no,mail_id):
+    print(name,"is palced")
+    print(roll_no,"registration no")
+    print("G mail_id",mail_id)
+    student_data(name="Hiamnsu",roll_no=154,mail_id="hk@13")  
+    
+    
